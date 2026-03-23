35f5c00a5349f92f8cdcc283fddfdfc601ecc40c
seonghyun <154983952+WHackerP@users.noreply.github.com>
2026-03-23 11:37:23 +0900

    Add files via upload

---
+++ code/3장/3_data_types.py
@@ -0,0 +1,13 @@
+# 3_data.types.py
+a = 10
+b = 3.1
+c = "hello"
+d = 'goodbye'
+e = True
+
+print(f"{a}: {type(a)}")  # f: format("서식 문자열 {변수}"), 띄어쓰기 싫으면 
+print(f"{b}: {type(b)}")
+print(f"{c}: {type(c)}")
+print(f"{d}: {type(d)}")
+print(f"{e}: {type(e)}")
+
